5d3a5d38c8513d4747cad757122003fa14bd87a6
Monica Sumie Hieda Dias <msumiedias@gmail.com>
2022-03-14 22:01:42 -0300

    att

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
-const path = require("path");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-    res.render("index");
+    const devList = ["Backend", "Frontend", "Fullstack", "DEvSEC"];
+  res.render("index", { titulo: "TESTE aula", nome: "thiago", devList: devList });
 });
 
 app.post("/", function (req, res) {
     res.send("Hello World via post");
-  });
+});
 
 app.get("/pagina", function (req, res) {
-    res.send("pagina2");
-  });
-
+    res.send("pagina 2");
+});
 
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
